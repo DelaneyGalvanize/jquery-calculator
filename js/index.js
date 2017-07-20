0ad4b4a606ee57c63dd0a203b0991e27e186f21f
DelaneyGalvanize <delaney.patterson001@gmail.com>
2017-07-20 12:19:53 -0600

    change background

--- js/index.js
+++ js/index.js
@@ -1,54 +1,60 @@
 $(document).ready(function() {
 
 
-  var $screen = $('#screen');
-  var displayText = $screen.text();
-  var currentOperator = '';
-  var currentTotal=0;
-  var numArray= [];
+    var $screen = $('#screen');
+    var displayText = $screen.text();
+    var currentOperator = '';
+    var currentTotal = 0;
+    var numArray = [];
 
 
-  $('.buttons').on('click', function() {
-      var input = $(event.target).text()
-      if (input >= 0 && input<10) {
-        displayText += input
-        $screen.text(displayText)
-      } else if (input === '+') {
-        currentOperator = '+'
-        displayText += input
-        $screen.text(displayText)
-      }  else if (input === 'C') {
-          displayText = '';
-          $screen.text('');
-      } else if (input === '-') {
-        currentOperator = '-'
-        displayText += input
-        $screen.text(displayText)
-      } else if (input === 'x') {
-        currentOperator = 'x'
-        displayText += input
-        $screen.text(displayText)
-      } else if (input === '÷') {
-        currentOperator = '÷'
-        displayText += input
-        $screen.text(displayText)
-      } else if (input === "=") {
-        numArray = displayText.split(/[x÷+-]/);
-          if (currentOperator === '+') {
-            displayText=Number(numArray[0])+ Number(numArray[1]);
+    $('.buttons').on('click', function() {
+        var input = $(event.target).text()
+        if (input >= 0 && input < 10) {
+            displayText += input
             $screen.text(displayText)
-        } else if (currentOperator === '-') {
-          displayText=Number(numArray[0])- Number(numArray[1]);
-          $screen.text(displayText)
-        } else if (currentOperator === 'x') {
-          displayText=Number(numArray[0]) * Number(numArray[1]);
-          $screen.text(displayText)
-        } else if (currentOperator === '÷') {
-          displayText=Number(numArray[0]) / Number(numArray[1]);
-          $screen.text(displayText)
-        }
+        } else if (input === '+') {
+            currentOperator = '+'
+            displayText += input
+            $screen.text(displayText)
+        } else if (input === 'C') {
+            displayText = '';
+            $screen.text('');
+        } else if (input === '-') {
+            currentOperator = '-'
+            displayText += input
+            $screen.text(displayText)
+        } else if (input === 'x') {
+            currentOperator = 'x'
+            displayText += input
+            $screen.text(displayText)
+        } else if (input === '÷') {
+            currentOperator = '÷'
+            displayText += input
+            $screen.text(displayText)
+        } else if (input === "=") {
+            numArray = displayText.split(/[x÷+-]/);
+            if (currentOperator === '+') {
+                displayText = (Number(numArray[0]) + Number(numArray[1]) + Number(numArray[2]));
+                $screen.text(displayText)
+            } else if (currentOperator === '-') {
+                displayText = Number(numArray[0]) - Number(numArray[1]);
+                $screen.text(displayText)
+            } else if (currentOperator === 'x') {
+                displayText = Number(numArray[0]) * Number(numArray[1]);
+                $screen.text(displayText)
+            } else if (currentOperator === '÷') {
+                if (Number(numArray[1]) == 0) {
+                    displayText = 'Error';
+                    $screen.text(displayText);
+                } else {
+                    displayText = Number(numArray[0]) / Number(numArray[1]);
+                    $screen.text(Number(displayText));
+                }
+            }
 
-}
-})
+
+        }
+    })
 
 })
